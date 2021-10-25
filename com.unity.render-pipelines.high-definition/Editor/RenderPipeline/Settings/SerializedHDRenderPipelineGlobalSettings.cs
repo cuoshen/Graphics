2d92db4ab9c98d0ad3a6956ee0eb040071093566
Adrien de Tocqueville <adrien.tocqueville@unity3d.com>
2021-10-25 19:07:18 +0200

    Migrate diffusion profiles from separate list to global volume

--- com.unity.render-pipelines.high-definition/Editor/RenderPipeline/Settings/SerializedHDRenderPipelineGlobalSettings.cs
+++ com.unity.render-pipelines.high-definition/Editor/RenderPipeline/Settings/SerializedHDRenderPipelineGlobalSettings.cs
@@ -43,7 +43,6 @@ namespace UnityEditor.Rendering.HighDefinition
 
         public SerializedProperty shaderVariantLogLevel;
         public SerializedProperty lensAttenuation;
-        public SerializedProperty diffusionProfileSettingsList;
         public SerializedProperty supportProbeVolumes;
         public SerializedProperty supportRuntimeDebugDisplay;
 
@@ -132,11 +131,6 @@ namespace UnityEditor.Rendering.HighDefinition
             shaderVariantLogLevel = serializedObject.Find((HDRenderPipelineGlobalSettings s) => s.shaderVariantLogLevel);
 
             lensAttenuation = serializedObject.FindProperty("lensAttenuationMode");
-            diffusionProfileSettingsList = serializedObject.Find((HDRenderPipelineGlobalSettings s) => s.diffusionProfileSettingsList);
-            m_DiffusionProfileUI = new DiffusionProfileSettingsListUI()
-            {
-                drawElement = DrawDiffusionProfileElement
-            };
             supportProbeVolumes = serializedObject.Find((HDRenderPipelineGlobalSettings s) => s.supportProbeVolumes);
             rendererListCulling = serializedObject.FindProperty("rendererListCulling");
 
